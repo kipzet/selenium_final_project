fcef8f67e598e5aaf9fc850afdacb6377edf5671
kipzet <zet.luchshiy@gmail.com>
2021-09-17 02:07:14 +0300

    User Register Realization

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,7 +12,7 @@ class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -65,3 +65,7 @@ class BasePage():
     def go_to_basket_page(self):
         link = self.browser.find_element(*BasketPageLocators.BASKET_LINK)
         link.click()
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     "probably unauthorised user"
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,32 @@ import pytest
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+import time
+
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = f'http://selenium1py.pythonanywhere.com/accounts/login/'
+        email = str(time.time()) + "@fakemail.org"
+        password = '54321ytrewq'
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        product = ProductPage(browser, link)
+        product.open()
+        product.add_to_basket()
+        product.should_be_added_product()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = f"http://selenium1py.pythonanywhere.com/ru/catalogue/hacking-exposed-wireless_208/"
+        product = ProductPage(browser, link)
+        product.open()
+        product.should_not_be_success_message()
 
 
 @pytest.mark.parametrize('number',
@@ -13,6 +39,7 @@ def test_guest_can_add_product_to_basket(browser, number):
     product.add_to_basket()
     product.solve_quiz_and_get_code()
     product.should_be_added_product()
+
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
